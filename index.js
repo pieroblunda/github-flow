0795bba2f283d323c941881942a37832978fec27
Piero <piero.blunda@[D.external@ynap.com>
2020-01-28 11:18:10 +0100

    Improving graph

--- index.js
+++ index.js
@@ -1,124 +1,142 @@
+// https://guides.github.com/introduction/flow/
+// https://github.com/nicoespeon/gitgraph.js/blob/master/packages/gitgraph-js/MIGRATE_FROM_GITGRAPH.JS.md
 // https://gitgraphjs.com/#8
 
 // Options
-const options = {
+const graphOptions = {
+  template: 'metro',
   orientation: 'horizontal',
-  mode: 'compact', // or compact if you don't want the messages
+  author: 'Piero Blunda',
+  mode: 'extended', // or compact
+  template: GitgraphJS.templateExtend('metro', {
+    branch: {
+      label: {
+        display: false
+      }
+    }
+  })
+};
+
+const branchOptions = {
+  style: {
+    label: {
+      display: false
+    }
+  }
 };
 
 // variables
 let container, graph, master, feature, featureB;
 
 // Graph 01: Master
-container = document.getElementById("scenario-01");
-graph = GitgraphJS.createGitgraph(container, options);
-master = graph.branch('master');
-master.commit('I').commit('II').commit('Three');
+container = document.getElementById('scenario-01');
+master = GitgraphJS.createGitgraph(container, graphOptions).branch(branchOptions);
+master.commit().commit().commit();
 
 // Graph 02: HotFix
-container = document.getElementById("scenario-02");
-master = GitgraphJS.createGitgraph(container, options).branch('master');
-master.commit('I').commit('II').commit('Three');
+container = document.getElementById('scenario-02');
+master = GitgraphJS.createGitgraph(container, graphOptions).branch(branchOptions);
+master.commit().commit().commit();
 
   feature = master.branch('featureA');
-  feature.commit('I');
+  feature.commit();
   master.merge(feature);
 
 // Graph 03: Un cambio menor que quiere el cliente (Cambiar un texto)
-container = document.getElementById("scenario-03");
-master = GitgraphJS.createGitgraph(container, options).branch('master');
-master.commit('I').commit('II');
+container = document.getElementById('scenario-03');
+master = GitgraphJS.createGitgraph(container, graphOptions).branch('master');
+master.commit().commit();
 
   // Feature A
   feature = master.branch('featureA');
-  feature.commit('I').commit('II');
+  feature.commit().commit();
   master.merge(feature);
 
 // Graph 04: Un cambio menor que quiere el cliente (Cambiar un texto)
-container = document.getElementById("scenario-04");
-master = GitgraphJS.createGitgraph(container, options).branch('master');
-master.commit('I').commit('II');
+container = document.getElementById('scenario-04');
+master = GitgraphJS.createGitgraph(container, graphOptions).branch('master');
+master.commit().commit();
 
   // Feature A
   feature = master.branch('featureA');
-  feature.commit('I').commit('II').commit('III').commit('IV').commit('V');
+  feature.commit().commit().commit().commit('IV').commit('V');
   master.merge(feature);
 
 // Graph 05: Dos cambios en serie
-container = document.getElementById("scenario-05");
-master = GitgraphJS.createGitgraph(container, options).branch('master');
-master.commit('I');
+container = document.getElementById('scenario-05');
+master = GitgraphJS.createGitgraph(container, graphOptions).branch('master');
+master.commit();
 
   // Feature A
   feature = master.branch('featureA');
-  feature.commit('I');
+  feature.commit();
   master.merge(feature);
   // Feature B
   feature = master.branch('featureB');
-  feature.commit('III').commit('IV');
+  feature.commit().commit('IV');
   master.merge(feature);
 
 // Graph 06: Dos nuevas funcionalidades en paralelo
-container = document.getElementById("scenario-06");
-master = GitgraphJS.createGitgraph(container, options).branch('master');
-master.commit('I');
+container = document.getElementById('scenario-06');
+master = GitgraphJS.createGitgraph(container, graphOptions).branch('master');
+master.commit();
 
   // Feature A
   feature = master.branch('featureA');
   featureB = master.branch('featureB');
 
-  feature.commit('I');
-  featureB.commit('II').commit('III').commit('IV').commit('V');
+  feature.commit();
+  featureB.commit().commit();
 
   master.merge(feature);
   master.merge(featureB);
 
 // Graph 07: Una funcionalidad que es desarrollada tiene una demora
-container = document.getElementById("scenario-07");
-master = GitgraphJS.createGitgraph(container, options).branch('master');
-master.commit('I');
+container = document.getElementById('scenario-07');
+master = GitgraphJS.createGitgraph(container, graphOptions).branch('master');
+master.commit();
 
   feature = master.branch('featureA');
   featureB = master.branch('featureB');
-  feature.commit('I').commit('II');
-  featureB.commit('II').commit('III').commit('IV');
+  feature.commit();
+  featureB.commit().commit();
   master.merge(feature);
   master.merge(featureB);
-  featureB.commit('I');
+  featureB.commit();
   master.merge(featureB);
 
 // Graph 07-B
-container = document.getElementById("scenario-07");
-master = GitgraphJS.createGitgraph(container, options).branch('master');
-master.commit('I');
+container = document.getElementById('scenario-07');
+master = GitgraphJS.createGitgraph(container, graphOptions).branch('master');
+master.commit();
 
   feature = master.branch('featureA');
   featureB = master.branch('featureB');
-  feature.commit('I').commit('II');
-  featureB.commit('II').commit('III').commit('IV');
+  feature.commit();
+  featureB.commit().commit();
   master.merge(feature);
   master.merge(featureB);
   feature = master.branch('featureC')
-  feature.commit('I');
+  feature.commit();
   master.merge(feature);
 
 // Graph 07 bis: Una funcionalidad que es desarrollada se desacarta
-container = document.getElementById("scenario-07");
-master = GitgraphJS.createGitgraph(container, options).branch('master');
-master.commit('I');
+container = document.getElementById('scenario-07');
+master = GitgraphJS.createGitgraph(container, graphOptions).branch('master');
+master.commit();
 
   feature = master.branch('featureA');
   featureB = master.branch('featureB');
-  feature.commit('I').commit('II');
-  featureB.commit('II').commit('III').commit('IV');
+  feature.commit();
+  featureB.commit().commit();
   master.merge(feature);
   master.merge(featureB);
-  featureB.commit('I');
+  featureB.commit();
 
 // Graph 08: Una tarea muy larga que no puede dividirse
-container = document.getElementById("scenario-08");
-master = GitgraphJS.createGitgraph(container, options).branch('master');
-master.commit('I');
+container = document.getElementById('scenario-08');
+master = GitgraphJS.createGitgraph(container, graphOptions).branch('master');
+master.commit();
 
   feature = master.branch('featureA');
   featureB = master.branch('featureB');
@@ -150,7 +168,7 @@ Estamos cambiando el branching model? No. En realidad estamos trabjando en horma
 // Consecuencias de trabajar de esta manera:
 - La reunion de sizing desaparece
 - La clumna Validation del pizarron desaparece
-- La columna "ready for code review" desaparece
+- La columna 'ready for code review' desaparece
 - Una tarjeta en la columna code coreview no deberia estar mas de 15 minutos
 - El branching model se simplifica
 - La SprintPlanning desaparece como reunion, y se trandforma en un comentario del PO en la standUp meeting. Solo es neceario un objetivo.
